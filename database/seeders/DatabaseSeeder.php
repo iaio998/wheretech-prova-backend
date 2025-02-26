bb356d068bad0d0b99afe6aba0e22428e82f4cb8
iaio998 <depalatisiacopo@gmail.com>
2025-02-26 17:18:42 +0100

    Added Test User for login in DB Changed db dump

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
         $this->call([
             AuthorSeeder::class,
             BookSeeder::class,
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Address;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,8 +14,16 @@ class UserSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        User::factory(1000)->create()->each(function ($user) {
+    {   
+        User::factory()->create([
+            'firstname' => 'Test',
+            'lastname' => 'Tester',
+            'email' => 't.tester@test.it',
+            'password' => Hash::make('querty'),
+            'remember_token' => 'test_token',
+        ]);
+
+        User::factory(999)->create()->each(function ($user) {
             $addresses = Address::inRandomOrder()->limit(rand(1, 3))->pluck('id');
 
             $user->addresses()->attach($addresses);
