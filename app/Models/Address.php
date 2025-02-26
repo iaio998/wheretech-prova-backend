9193028c262128f680eed8ce8c00b8bbbff2ad42
iaio998 <depalatisiacopo@gmail.com>
2025-02-26 15:51:09 +0100

    Created Address - Model, Migration, Factory and Seeder (tested db)

---
+++ app/Models/Address.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'street',
+        'city',
+        'zipcode',
+    ];
+}
